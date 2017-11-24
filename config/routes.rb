198c2830ee8fc08577432e0b8da5c38826d48d08
Joevin Canet <canetjoevin@gmail.com>
2017-11-24 16:51:19 +0100

    day2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: [:index, :create, :show, :new]
+  resources :cocktails, only: [:index, :create, :show, :new] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
 
